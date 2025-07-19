d27bff32c9904d23b075b967140fa27211863bae
Noel Kurian <nbkurian11@gmail.com>
2025-07-19 16:14:53 -0400

    worked on database

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Enter username'],
+        unique: true,
+        trim: true,
+        minlength: [5, 'Username must be at least 5 characters long']
+    },
+    password: {
+        type: String,
+        required: [true, 'Enter password'],
+        minlength: [8, 'Password must be at least 8 characters long']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
