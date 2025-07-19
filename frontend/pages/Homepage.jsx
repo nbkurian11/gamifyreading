72b3caf818d5b8edfca55051b54ebb6b411dd051
Rahbir1518 <rahbir1518@gmail.com>
2025-07-19 00:11:39 -0400

    routing

---
+++ frontend/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LeaderboardComponent from "../components/LeaderboardComponent.jsx"
+const Homepage = () => {
+  return (
+    <div>
+      <LeaderboardComponent/>
+    </div>
+  )
+}
+
+export default Homepage
