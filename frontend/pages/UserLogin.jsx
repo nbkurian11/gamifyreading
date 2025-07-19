581aa6552957198e6fa3bc2b810fba0dfd0b7cdb
mikaelacadiz-desn <mikaelacadiz.mc@gmail.com>
2025-07-19 10:13:25 -0400

    bg

--- frontend/pages/UserLogin.jsx
+++ frontend/pages/UserLogin.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const UserLogin = () => {
   return (
-    <div>
-      User
+    <div className="min-h-screen bg-gradient-to-b from-[#F1EDDD] to-[#EED4B3] p-6">
+      <h1 className="text-white text-3xl">user</h1>
+
+      <div className="flex items-center justify-center min-h-screen">
+      <p className="text-center">Gami</p>
+      </div>
     </div>
   )
 }
