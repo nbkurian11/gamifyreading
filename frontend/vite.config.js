5e7d48ac205a2136db89bf6399d3c73ce0a361d6
mikaelacadiz-desn <mikaelacadiz.mc@gmail.com>
2025-07-19 19:03:35 -0400

    loginUI

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [tailwindcss(),react(),],
+  plugins: [
+    tailwindcss(),
+  ],
 })
+
