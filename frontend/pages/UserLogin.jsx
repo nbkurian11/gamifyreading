7156bf5862ae9735d6bcde7af44e527e38b80b4e
Noel Kurian <nbkurian11@gmail.com>
2025-07-20 04:18:32 -0400

    improved leaderboard

--- frontend/pages/UserLogin.jsx
+++ frontend/pages/UserLogin.jsx
@@ -119,7 +119,10 @@ const UserLogin = () => {
                 </button>
                 <div className="text-center">
                   <p className="text-xs text-[#98673E]">
-                    Start typing in any field to go to homepage
+                    "If you don't like to read, you haven't found the right book."
+— J.K. Rowling
+
+
                   </p>
                 </div>
               </div>
